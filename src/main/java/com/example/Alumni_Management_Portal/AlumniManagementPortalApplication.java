10d0316060b0ee5a7972612b193049b2b1f41b4f
pavelGorod99 <pavel.gorodetchi@gmail.com>
2023-07-30 14:59:50 -0500

    start-up

---
+++ src/main/java/com/example/Alumni_Management_Portal/AlumniManagementPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Alumni_Management_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniManagementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniManagementPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Alumni_Management_Portal/AlumniManagementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alumni_Management_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniManagementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
